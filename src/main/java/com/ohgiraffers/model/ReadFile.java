44f8a6cb3c52ccafeaff1c8c58da408ac0220f2c
manje cho <whakswp123@naver.com>
2023-06-09 13:11:50 +0900

    ReadFile 객체 추가(#1)

---
+++ src/main/java/com/ohgiraffers/model/ReadFile.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ReadFile {
+    private static final ReadFile readfile = new ReadFile();
+
+    private ReadFile() {
+    }
+
+    public static ReadFile getReadfile() {
+        return readfile;
+    }
+
+    public String readUserDB() throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new FileReader("C:\\Users\\user\\Desktop\\userDB.txt"
+                        , StandardCharsets.UTF_8)
+        );
+        String str;
+        String db = "";
+
+        while ((str = reader.readLine()) != null) {
+            if (db.equals("")) {
+                db = db + str;
+            }
+            db = db + "," + str;
+            System.out.println(db);
+        }
+
+        reader.close();
+        return db;
+    }
+}
