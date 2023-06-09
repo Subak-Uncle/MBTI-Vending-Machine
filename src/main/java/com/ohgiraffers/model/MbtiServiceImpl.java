212cb831988576cf28b86d185bcd7eddeee42c4d
manje cho <whakswp123@naver.com>
2023-06-09 10:12:24 +0900

    MbtiServiceImpl 파일 싱글톤으로 변경(#1)

--- src/main/java/com/ohgiraffers/model/MbtiServiceImpl.java
+++ src/main/java/com/ohgiraffers/model/MbtiServiceImpl.java
@@ -3,6 +3,15 @@ package com.ohgiraffers.model;
 import com.ohgiraffers.dto.UserDto;
 
 public class MbtiServiceImpl implements MbtiService {
+    private static MbtiServiceImpl mbtiService = new MbtiServiceImpl();
+
+    private MbtiServiceImpl() {
+    }
+
+    public static void getMbtiService(MbtiServiceImpl mbtiService) {
+        MbtiServiceImpl.mbtiService = mbtiService;
+    }
+
     @Override
     public void login(String userId, String userPw) {
         UserDto.getUserDto().checkId(userId);
