a80eeae01b41f787a2aab6206acbcab9340de04f
Son Jung In <nanajungin13@gmail.com>
2023-06-11 14:10:29 +0900

    AnswerController 질문jsp 이동

--- src/main/java/com/ohgiraffers/controller/AnswerController.java
+++ src/main/java/com/ohgiraffers/controller/AnswerController.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.controller;
 
 import com.ohgiraffers.dto.User;
+import com.ohgiraffers.model.MbtiDecider;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,34 +10,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/answer")
 public class AnswerController extends HttpServlet {
+    private int questionIndex = 1;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String answer = "I";
-        System.out.println(req.getAttribute("error"));
+        RequestDispatcher rd;
+        MbtiDecider mbtiDecider=new MbtiDecider();
+        String answer=req.getParameter("answer");
 
-        if (req.getAttribute("error").equals("아이디 에러")) {
-            resp.sendError(400, "아이디/비밀번호 에러");
+        System.out.println(answer);
+
+        mbtiDecider.Decider(answer);
+        questionIndex++;
+
+        System.out.println(questionIndex);
+
+        if(questionIndex==13){
+            rd=req.getRequestDispatcher("/WEB-INF/views/result.jsp");
+            rd.forward(req,resp);
         }
-        User user;
-        user = (User) req.getAttribute("user");
-        String error = (String) req.getAttribute("error");
-
-        System.out.println(user.getUserId());
-        System.out.println(user.getUserPw());
-
-
-        if (error.equals("아이디 에러") || user.equals("비밀번호 에러")) {
-            resp.sendError(400, "아이디/비밀번호 에러");
+        else{
+            rd=req.getRequestDispatcher("/WEB-INF/views/question"+questionIndex+".jsp");
         }
-        //getParameter로 입력값 전달 받기
-//        else{
-//            MbtiDecider mbtiDecider=new MbtiDecider();
-//            mbtiDecider.Decider(answer);
-//        }
+
+        rd.forward(req,resp);
 
     }
 }
